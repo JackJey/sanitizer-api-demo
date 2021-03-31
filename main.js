f22f2c85f8183a5c62310f6cc31783212cafc37b
JackJey <69510203+JackJey@users.noreply.github.com>
2021-03-31 18:02:08 +0900

    init

---
+++ main.js
@@ -0,0 +1,46 @@
+'use strict';
+const $  = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+EventTarget.prototype.on = EventTarget.prototype.addEventListener
+
+document.on('DOMContentLoaded', async (e) => {
+  console.log(e)
+  $('form').on('submit', (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const raw  = formData.get('rawtext')
+    const mode = formData.get('mode')
+
+    console.log(mode)
+
+    const option = ((mode) => {
+      switch (mode) {
+        case 'default':
+          return {}
+        case 'no-attr':
+          return {allowAttributes: []}
+        case 'no-elem':
+          return {allowElements: []}
+      }
+    })(mode)
+
+    console.log(raw)
+    const sanitizer = new Sanitizer(option)
+
+    const sanitized = sanitizer.sanitizeToString(raw)
+    console.log(sanitized)
+    $('#sanitized').value = sanitized
+
+    const result = new Sanitizer().sanitize(raw);
+    console.log(result)
+
+    $('#render').innerHTML = ''
+    $('#render').appendChild(result)
+  })
+
+  $('#copy').on('click', async () => {
+    const code = $('#code pre code').textContent
+    await navigator.clipboard.writeText(code);
+  })
+})
