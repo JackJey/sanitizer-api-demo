0121fe6182241112c2180ecac3ef4437f6513bcb
JackJey <69510203+JackJey@users.noreply.github.com>
2021-04-02 16:28:35 +0900

    cleanup

--- main.js
+++ main.js
@@ -3,33 +3,31 @@ const $  = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 EventTarget.prototype.on = EventTarget.prototype.addEventListener
 
+function mode2option(mode) {
+  switch (mode) {
+    case 'default':
+      return {}
+    case 'no-attributes':
+      return {allowAttributes: []}
+    case 'no-element':
+      return {allowElements: []}
+    case 'allow-iframe':
+      return {allowElements: ['iframe']}
+    default:
+      return {}
+  }
+}
+
 document.on('DOMContentLoaded', async (e) => {
 
   const feature_detection = (window.Sanitizer !== undefined)
   $('#feature-detection').textContent = feature_detection ? "ENABLED" : "DISABLED"
-
-
-  function mode2option(mode) {
-    switch (mode) {
-      case 'default':
-        return {}
-      case 'no-attributes':
-        return {allowAttributes: []}
-      case 'no-element':
-        return {allowElements: []}
-      case 'allow-iframe':
-        return {allowElements: ['iframe']}
-      default:
-        return {}
-    }
-  }
 
   $('#mode').on('change', (e) => {
     const mode = e.target.value
     const option = mode2option(mode)
     const src = `new Sanitizer(${JSON.stringify(option)})`
     $('#src pre code').textContent = src
-    console.log(src)
   })
   $('#mode').dispatchEvent(new Event('change'));
 
@@ -37,12 +35,9 @@ document.on('DOMContentLoaded', async (e) => {
     e.preventDefault()
 
     const formData = new FormData(e.target)
-    const raw  = formData.get('rawtext')
-    const mode = formData.get('mode')
-
-    console.log(mode)
-
-    const option = mode2option(mode)
+    const raw      = formData.get('rawtext')
+    const mode     = formData.get('mode')
+    const option   = mode2option(mode)
 
     console.log(raw)
     const sanitizer = new Sanitizer(option)
